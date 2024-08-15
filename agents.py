f279ef7d6e85f952b21249c2451db553defcdadd
mayank1365 <mayank1365@gmail.com>
2024-08-16 02:12:08 +0530

    project setup

---
+++ agents.py
@@ -0,0 +1,32 @@
+from crewai import Agent
+from textwrap import dedent
+from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
+
+
+class CustomAgents:
+    def __init__(self):
+        self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+        # self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
+
+    def agent_1_name(self):
+        return Agent(
+            role="Define agent 1 role here",
+            backstory=dedent(f"""Define agent 1 backstory here"""),
+            goal=dedent(f"""Define agent 1 goal here"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def agent_2_name(self):
+        return Agent(
+            role="Define agent 2 role here",
+            backstory=dedent(f"""Define agent 2 backstory here"""),
+            goal=dedent(f"""Define agent 2 goal here"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
