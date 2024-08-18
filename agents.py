5983f626782005f861f889d46f4e23dd71dd584b
mayank1365 <mayank1365@gmail.com>
2024-08-18 13:49:52 +0530

    Defined the agents required

--- agents.py
+++ agents.py
@@ -1,32 +1,58 @@
 from crewai import Agent
 from textwrap import dedent
-from langchain.llms import OpenAI
 from langchain_openai import ChatOpenAI
-
+from tools.search_tools import SearchTools
+from tools.interpreter_tools import InterpreterTools
 
 class CustomAgents:
     def __init__(self):
-        self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
-        # self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
+        # self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+        self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
 
-    def agent_1_name(self):
+    def expert_health_analyst(self):
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
-            # tools=[tool_1, tool_2],
+            role="Expert Health Analyst",
+            backstory=dedent(f"""
+                                Expert in summarizing medical reports, particularly blood test results,
+                                with a focus on making complex data understandable for better health decisions.
+                            """),
+            goal=dedent(f"""
+                            Provide feedback on how to improve health based on the research findings.
+                        """),
+            tools=[SearchTools.search_internet ],
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
+            llm=self.OpenAIGPT4,
         )
 
-    def agent_2_name(self):
+    def medical_report_interpreter(self):
         return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
-            # tools=[tool_1, tool_2],
+            role="Medical Report Interpreter",
+            backstory=dedent(f"""
+                                Over a decade of experience in gathering and analyzing credible medical information online,
+                                transforming complex data into actionable health insights.
+                            """),
+            goal=dedent(f"""
+                            Summarize the blood test report and search the internet for related information.                            
+                        """),
+            tools=[SearchTools.search_internet , InterpreterTools.pdf_to_text_tool],
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
+            llm=self.OpenAIGPT4,
+        )
+    
+    def health_information_researcher(self):
+        return Agent(
+            role="Health Information Researcher",
+            backstory=dedent(f"""
+                                Specialist in nutrition and lifestyle, 
+                                offering tailored advice to enhance health based on precise analysis and research findings.
+                             """),
+            goal=dedent(f"""
+                            Conduct in-depth research on medical conditions, health metrics, and lifestyle improvements.                            
+                        """),
+            tools=[SearchTools.search_internet],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT4,
         )
